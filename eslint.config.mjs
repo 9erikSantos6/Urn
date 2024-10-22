b4316fdbd874f1d3d83d6474fa3f9cfa67e6a249
9erikSantos6 <9xerikx6@gmail.com>
2024-10-22 16:16:44 -0300

    chore: Reconfiguring Prettier and Eslint, Jest removed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
 ];
