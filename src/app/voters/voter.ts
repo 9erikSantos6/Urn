e63f6e3bd3c290fda8442722e0bbce4e2aeafc99
9erikSantos6 <9xerikx6@gmail.com>
2024-10-22 17:08:53 -0300

    refactor: Formatted by prettier

--- src/app/voters/voter.ts
+++ src/app/voters/voter.ts
@@ -10,13 +10,13 @@ export class Voter {
   private toCpfFormat(value: string): string {
     return `${value.slice(0, 3)}.${value.slice(3, 6)}.${value.slice(
       6,
-      9
+      9,
     )}-${value.slice(9, 11)}`;
   }
 
   private validateCPF(value: string): string {
     const cpf: string = value.replace(/\D/g, '');
-    if (cpf.length === 11 && !isNaN(cpf as any) && !isNaN(parseFloat(cpf))) {
+    if (cpf.length === 11) {
       return this.toCpfFormat(cpf);
     }
     throw new Error('Invalid CPF!');
